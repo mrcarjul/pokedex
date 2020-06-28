ea97d03623aa367cbe87d2966b92a1d2933e21e7
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-28 18:55:24 -0500

    initial version of api configuration

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_URL = 'https://pokeapi.co/api/v2';
+
+/**
+ * @description Creates an Axios Instance
+ * @param baseURL Default api uri used if not given
+ */
+const create = (baseURL: string = API_URL) => {
+  const api = axios.create({
+    baseURL,
+    timeout: 25000,
+  });
+
+  /**
+   * @description Requests Pokemons Data
+   * @param {number} offset position for pagination
+   */
+  const getPokemons = (offset: number) =>
+    api.get(`/pokemon?offset=${offset}&limit=54'`);
+
+  /**
+   * @description Requests Pokemon detailed Data
+   * @param {number} id Pokemon id
+   */
+  const getPokemonData = (id: string) => api.get(`/pokemon/${id}`);
+
+  return {
+    getPokemons,
+    getPokemonData,
+  };
+};
+
+const API = create();
+
+export default API;
