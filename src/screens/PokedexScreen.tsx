bbb3af0505a355a2ab1a132434f67bc05436ea39
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-28 20:39:09 -0500

    empty version of screen without redux work flow

--- src/screens/PokedexScreen.tsx
+++ src/screens/PokedexScreen.tsx
@@ -1,18 +1,88 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 // Core
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, PixelRatio, StyleSheet, Text, View} from 'react-native';
+
+// Personalized Components
+import {PokeHeader, PokeSection, PokeButton} from '../components';
+
+// Hooks
+import {useDebouncedCallback} from '../hooks';
 
 // Theme
-import {genericStyles} from '../theme';
+import {colors, genericStyles, metrics} from '../theme';
+
+// Utils
+import {NavigationStackScreenProps} from 'react-navigation-stack';
+
+// Service
+import API from '../services/api';
+
+const ITEM_HEIGHT = PixelRatio.roundToNearestPixel(metrics.width / 3 - 20);
 
 /**
  * @description Screen to display Pokedex
  */
-function PokedexScreen() {
+function PokedexScreen({navigation}: NavigationStackScreenProps) {
+  const flatListRef = useRef(null);
+  const [offset, setOffset] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+
+  const updateOffset = useCallback(() => {
+    setOffset(offset + 54);
+  }, [offset]);
+
+  const getPokemons = async () => {
+    const response = await API.getPokemons(offset);
+    if (response && response.data) {
+      const {results} = response.data;
+      setPokemons(results);
+      updateOffset();
+    }
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  /**
+   * @description default items layout calculations
+   */
+  const getItemLayout = useCallback((data, index) => {
+    return {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index};
+  }, []);
+
+  /**
+   * @description builds the item key for FlatList
+   */
+  const getKey = useCallback(({name}) => `Pokemon-${name}`, []);
+
+  const renderItem = useCallback(({item}) => {
+    const splitteduri = item.url.split('/');
+    const pokemonId = splitteduri[splitteduri.length - 2];
+    return <PokeSection pokemonId={pokemonId} name={item.name} />;
+  }, []);
+
   return (
-    <View style={[styles.container, styles.centerContents]}>
-      <Text>PokedexScreen</Text>
+    <View style={styles.container}>
+      <PokeHeader centerTxt="Pokedex" />
+      <View style={styles.container}>
+        <FlatList
+          keyboardShouldPersistTaps="always"
+          keyboardDismissMode="on-drag"
+          data={pokemons}
+          extraData={pokemons}
+          getItemLayout={getItemLayout}
+          initialNumToRender={10}
+          keyExtractor={getKey}
+          maxToRenderPerBatch={10}
+          renderItem={renderItem}
+          ref={flatListRef}
+          windowSize={5}
+          numColumns={3}
+        />
+      </View>
     </View>
   );
 }
